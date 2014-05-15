ee591154f86ac3d27c7a63f5842b8d3b54918a46
Samuel Cochran <sj26@sj26.com>
2014-05-15 18:00:41 +1000

    jQuery.noconflict belongs with the load call

--- dotjs.user.js
+++ dotjs.user.js
@@ -9,13 +9,12 @@
 
 (function () {
   function getScript(uri, callback) {
-    var script = document.createElement('script');
-    var head   = document.head || document.getElementsByTagName( "head" )[0] || document.documentElement;
+    var script = document.createElement("script");
+    var head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
 
-    script.async  = 'async';
-    script.src    = uri;
+    script.async = "async";
+    script.src = uri;
     script.onload = function onload() {
-      jQuery.noConflict();
       callback();
     };
 
@@ -24,19 +23,20 @@
 
   function dotjs($) {
     $.ajax({
-      url: 'http://localhost:3131/' + window.location.host.replace('www.','') + '.js',
-      dataType: 'text',
+      url: "http://localhost:3131/" + window.location.host.replace("www.","") + ".js",
+      dataType: "text",
       success: function(d){
         $(function(){ eval(d); });
       },
       error: function(){
-        console.log('no dotjs server found at localhost:3131');
+        console.log("no dotjs server found at localhost:3131");
       }
     })
   }
 
   if (!window.jQuery) {
-    getScript('http://localhost:3131/jquery.js', function() {
+    getScript("http://localhost:3131/jquery.js", function() {
+      jQuery.noConflict();
       dotjs(jQuery);
     });
   }
